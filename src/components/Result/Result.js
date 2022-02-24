3080304489e207801e40fd4ba9bc22d6603c094d
masudx08 <promasud225@gmail.com>
2022-02-24 16:53:31 +0600

    full complete with responsive

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -123,21 +123,19 @@ export default function Result() {
           )
         })
       }
-      {/* <div id='pagination' className='d-flex justify-content-center'>
-      <Pagination>
-        <Pagination.First />
-        <Pagination.Prev />
-        <Pagination.Item active>{1}</Pagination.Item>
-        <Pagination.Item>{2}</Pagination.Item>
-        <Pagination.Item>{3}</Pagination.Item>
-        <Pagination.Item >{4}</Pagination.Item>
-        <Pagination.Item>{5}</Pagination.Item>
-        <Pagination.Ellipsis />
-        <Pagination.Item>{10}</Pagination.Item>
-        <Pagination.Next />
-        <Pagination.Last />
-    </Pagination>
-      </div> */}
+      <div id='pagination' className='d-flex justify-content-center'>
+        <Pagination>
+          <Pagination.Prev />
+          <Pagination.Item active>{1}</Pagination.Item>
+          <Pagination.Item>{2}</Pagination.Item>
+          <Pagination.Item>{3}</Pagination.Item>
+          <Pagination.Item >{4}</Pagination.Item>
+          <Pagination.Item>{5}</Pagination.Item>
+          <Pagination.Ellipsis />
+          <Pagination.Item>{10}</Pagination.Item>
+          <Pagination.Next />
+      </Pagination>
+      </div>
      </div>
     </Container>
   )
